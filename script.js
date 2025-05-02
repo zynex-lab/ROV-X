25306981045732e43a21139dab1dfb0420251c13
zynxer <113895823+zynex-lab@users.noreply.github.com>
2025-05-02 18:27:27 +0700

    script.js を更新

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ document.onkeydown = function(e) {
     return false;
   }
 }
+
+const elements = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+elements.forEach(el => observer.observe(el));
